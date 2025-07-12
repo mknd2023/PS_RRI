2546c6ebdf766af4ea6ce57fd90c74a7ef6547a3
mknd2023 <f20230504@goa.bits-pilani.ac.in>
2025-07-12 22:33:52 +0530

    ADC Characterization

---
+++ ADC Characterization/adc_char.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fft import fft
+adcval=[]
+sampno=np.linspace(0, 32767, 32768)
+f=open("Mhz30.txt","r")
+for row in f:
+    row=row.split('\n')
+    adcval.append(float(row[0]))
+f.close()
+sampno8k=np.linspace(0, 8191, 8192)
+vals8k=[]
+for i in range(0,4):
+    addi=[]
+    addi=adcval[i*8192:(i+1)*8192]
+    vals8k.append(addi)
+    i=i+1
+fftaddi=np.zeros(8*1024)
+for j in vals8k:
+    fftaddi=fftaddi+(np.square(np.abs(fft(j))))
+fftaddi=np.divide(fftaddi, 4)
+plt.plot(sampno8k, 10*np.log10(fftaddi), marker='x')
+plt.show()
+nad=[]
+for i in fftaddi:
+    nad.append(i)
+nad[120]=0
+nad[8072]=0
+x=np.sum(nad)
+sinad=10*np.log10(fftaddi[120]*2/x)
+print("SINAD: ", sinad,"dB")
+enob=(sinad-1.76)/6.02
+print("ENOB: ", enob, "bits")
+sfdr=10*np.log10(fftaddi[120]/np.max(nad))
+print("SFDR: ", sfdr, "dB")
+count=2
+while (120*count<4096):
+    nad[120*count]=0
+    nad[8192-120*count]=0
+    count=count+1
+x=np.sum(nad)
+snr=10*np.log10(fftaddi[120]*2/x)
+print("SNR: ", snr, "dB")
